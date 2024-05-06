ad4e481b3d603fbd0de483b7a87419e0026644a1
Ydna <0xEFEFEF@gmail.com>
2024-05-06 20:03:08 +0800

    fix: public acl

--- Sources/ComposableArchitecture/_Extensions/Reducer/DependencyOverride.swift
+++ Sources/ComposableArchitecture/_Extensions/Reducer/DependencyOverride.swift
@@ -6,7 +6,7 @@ extension Reducer {
 	/// - Parameter key: The key for the value to set in the dependencies.
 	/// - Parameter value: The value to set for this value's type in the dependencies.
 	/// - Returns: A reducer that has the given value set in its dependencies.
-	func dependency<Key: TestDependencyKey>(
+	public func dependency<Key: TestDependencyKey>(
 		_ key: Key.Type,
 		_ value: Key.Value
 	) -> _DependencyKeyWritingReducer<Self> {
