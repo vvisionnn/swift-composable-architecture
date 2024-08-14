1854aafd003d86ed854afe5d8522ac13ef82f216
Ydna <0xEFEFEF@gmail.com>
2024-08-15 00:40:23 +0800

    fix: resolve present continuation leak using runloop

--- Sources/ComposableArchitecture/UIKit/ViewControllerPresentable.swift
+++ Sources/ComposableArchitecture/UIKit/ViewControllerPresentable.swift
@@ -53,7 +53,7 @@ extension ViewControllerPresentable {
 		return store.publisher
 			.removeDuplicates(by: { toID($0) == toID($1) })
 			.withPrevious()
-			.receive(on: DispatchQueue.main)
+			.receive(on: RunLoop.main)
 			.sink { [weak self] (prevState, presentationState) in
 				guard let self else { return }
 				queue.enqueue { @MainActor in
